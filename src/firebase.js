39ae79fc0f6ab285a8f30f4e26a1dd790b72b3be
BhavyaSahithi1423 <bhavya.sahithi6@gmail.com>
2025-06-26 15:14:20 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCBbZUq36yEbn1Ps-ByEFVU9zB5l7soiVo",
-  authDomain: "safespace-5c550.firebaseapp.com",
-  projectId: "safespace-5c550",
-  storageBucket: "safespace-5c550.appspot.com", 
-  messagingSenderId: "238810384517",
-  appId: "1:238810384517:web:d771c6de3c4bac785cc1ec"
+    /***/
 };
 
 const app = initializeApp(firebaseConfig);
